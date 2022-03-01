c0df07b41f398a8cdc6187e2a8eed5b7ad6f1d77
Ahmad <03-134181-014@student.bahria.edu.pk>
2022-03-01 17:35:01 +0500

    Day 2 Work

---
+++ lib/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    int myapp = 2;
+    String app = "First App";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+        foregroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $myapp $app"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
