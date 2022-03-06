3e29679cf124a69749087c7adf1b0822c0c20b0c
Ahmad <03-134181-014@student.bahria.edu.pk>
2022-03-07 00:27:15 +0500

    7 mar commit done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_myapp/pages/home_page.dart';
 import 'package:flutter_myapp/pages/login_page.dart';
+import 'package:flutter_myapp/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -23,8 +24,8 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(brightness: Brightness.dark),
       initialRoute: '/login',
       routes: {
-        '/': (context) => HomePage(),
-        '/login': (context) => LoginPage()
+         MyRoutes.homeRoute: (context) => HomePage(),
+         MyRoutes.loginRoute: (context) => LoginPage()
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_myapp/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -8,47 +9,53 @@ class LoginPage extends StatelessWidget {
     return Material(
         color: Colors.white,
         child: Center(
-            child: Column(children: [
-          SizedBox(
-            height: 40.0,
-          ),
-          Image.asset("assets/images/login_image.png", fit: BoxFit.cover),
-          SizedBox(
-            height: 5.0,
-          ),
-          Text("Welcome to Login",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          SizedBox(
-            height: 10.0,
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Please Enter Email", labelText: "Email"),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      hintText: "Please Enter Password", labelText: "Password"),
-                ),
-                SizedBox(
-                  height: 25.0,
-                ),
-                ElevatedButton(
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(
-                      shadowColor: Colors.black
-                    ),
-                    onPressed: () {
-                      print("Hi");
-                    })
-              ],
+            child: SingleChildScrollView(
+          child: Column(children: [
+            SizedBox(
+              height: 40.0,
             ),
-          )
-        ])));
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+              scale: 2.7,
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Text("Welcome to Login",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Please Enter Email", labelText: "Email"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Please Enter Password",
+                        labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 35.0,
+                  ),
+                  ElevatedButton(
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(
+                          shadowColor: Colors.black, minimumSize: Size(30, 60)),
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      })
+                ],
+              ),
+            )
+          ]),
+        )));
   }
 }
